21578578841caad1ecc012c0111d940522ee2335
AbdulQader Haddad <abedhadad998@gmail.com>
2024-03-13 06:33:02 +0200

    Delete user and simulate logout

--- script.js
+++ script.js
@@ -167,6 +167,23 @@ btnTransfer.addEventListener('click', function(e) {
   
 });
 
+btnClose.addEventListener('click', function(e) {
+  e.preventDefault(); // Prevent button from submitting form. 
+  
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    
+    accounts.splice(index, 1); // Mutates array. 
+    
+    //Hide UI - simulate logout
+    containerApp.style.opacity = 0;
+  };
+})
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
